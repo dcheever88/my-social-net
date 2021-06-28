e034e4a890a54b42879c4a4150b0526a3ea5f81b
dcheever <cheeverdanny@gmail.com>
2021-06-27 19:26:55 -0500

    create User model

--- models/User.js
+++ models/User.js
@@ -1,7 +1,49 @@
-const { Schema, Model } = require('mongoose');
+const { Schema, model } = require('mongoose');
+require('mongoose-type-email');
 
 const UserSchema = new Schema({
     username: {
         type: String,
-    }
-})
+        required: true,
+        unique: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        work: Mongoose.SchemaTypes.Email,
+        home: Mongoose.SchemaTypes.Email
+    },
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            re: 'Thought'
+        }
+    ],
+    friends: [
+        {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+        }
+    ]
+},
+{
+    toJSON: {
+        virtuals: true,
+        getters: true
+    },
+    id: false
+}
+);
+
+// create a virtual for total friends for User
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
+
+// create new User model using UserSchema
+const User = model ('User', UserSchema);
+
+// export User model
+module.exports = User;
